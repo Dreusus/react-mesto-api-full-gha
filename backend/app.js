fabae87f803f19317ac00d29a928f5b93fec7347
unknown <pitakxdd@gmail.com>
2023-04-03 22:45:16 +0300

    mongoDB

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,7 @@ const app = express();
 app.use(cors());
 
 const { PORT = 3000 } = process.env;
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  family: 4,
-});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
